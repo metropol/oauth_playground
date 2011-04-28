29792a26d9ad9687557a07095c51d6c76c0fdb46
Ugo Di Profio <gmetropol@gmail.com>
2011-04-28 15:05:13 +0900

    Initial Commit. Ran rake db:migrate and rake test.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthDemo::Application.config.secret_token = 'b95c1f5a982f7d71fc496c71bfaf33b67d941302d123a54960758bf5570832b49a34b3e85152556ab749620406e9f0199ce0a71ba2db4208785010bfccd97e86'
